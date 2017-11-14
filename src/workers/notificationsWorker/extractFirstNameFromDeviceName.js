5f3ed1887ce2bab7cd8cd2d07ca09b2d2bccd301
Julia Qiu <juliaqiuxy@gmail.com>
2017-11-14 14:30:50 -0800

    add first name to notification and implement different alert messages

---
+++ src/workers/notificationsWorker/extractFirstNameFromDeviceName.js
@@ -0,0 +1,15 @@
+import femaleNames from 'datasets-female-first-names-en';
+import maleNames from 'datasets-male-first-names-en';
+
+export const extractFirstNameFromDeviceName = (string) => {
+  const trimedString = string.split(' ')[0].split('\'s')[0];
+  const femaleName = femaleNames.find(name => trimedString.toLowerCase() === name.toLowerCase());
+  const maleName = maleNames.find(name => trimedString.toLowerCase() === name.toLowerCase());
+
+  let name = femaleName ? femaleName : maleName;
+  if (!name) {
+    name = null;
+  }
+
+  return name;
+};
--- src/workers/notificationsWorker/index.js
+++ src/workers/notificationsWorker/index.js
@@ -1,11 +1,15 @@
 import ResortService from '../../services/ResortService';
 import UserDeviceService from '../../services/UserDeviceService';
 import NotificationService from '../../services/NotificationService';
+import { extractFirstNameFromDeviceName } from './extractFirstNameFromDeviceName';
+
+//3600 is an hour in epoch time
+const EPOCH_HOUR = 3600;
 
 const run = async (metadata) => {
 
   const resortService = new ResortService();
-  const lastSnow = await resortService.getSnowMetadata();
+  const lastSnow = resortService.getSnowMetadata;
 
   if (lastSnow.snowLastSeen) {
     await resortService.setSnowMetadata({
@@ -15,13 +19,30 @@ const run = async (metadata) => {
     const userDeviceService = new UserDeviceService();
     const userDevices = await userDeviceService.getUserDevices();
 
+    const now = new Date();
+    const epoch = now.getTime();
+
+    const snowedWithinAnHour = Math.abs(epoch - lastSnow.snowLastSeen) <= EPOCH_HOUR;
+    const snowedWithinAnHourMessage = "Hi Five! It's snowing in Tahoe. ❄️"
+    const snowedNotWithinAnHourMessage = "It's your lucky day. It snowed in Tahoe."
+
+    const message = snowedWithinAnHour ?
+    snowedWithinAnHourMessage :
+    snowedNotWithinAnHourMessage;
+
     // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
-    const notifications = userDevices.map(userDevice => ({
-      to: userDevice.notificationToken,
-      sound: 'default',
-      body: 'Yay! It\'s snowing in Tahoe',
-      data: { withSome: 'data' },
-    }));
+    const notifications = userDevices.map(userDevice => {
+      const userFirstName = extractFirstNameFromDeviceName(userDevice.deviceName) ?
+      ` ${extractFirstNameFromDeviceName(userDevice.deviceName)}` :
+      '';
+
+      return ({
+        to: userDevice.notificationToken,
+        sound: 'default',
+        body: `Hey${userFirstName}, ${message}`,
+        data: { withSome: 'data' },
+      })
+    });
 
     const notificationService = new NotificationService();
     const receipts = await notificationService.broadcast(notifications);
