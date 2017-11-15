1eb5903ebe68b94acdd665ffe5bf82a0c5f7b222
Julia Qiu <juliaqiuxy@gmail.com>
2017-11-14 16:58:31 -0800

    check for "undefined"

--- src/workers/notificationsWorker/__tests__/extractFirstNameFromDeviceName-test.js
+++ src/workers/notificationsWorker/__tests__/extractFirstNameFromDeviceName-test.js
@@ -3,83 +3,88 @@ import { extractFirstNameFromDeviceName } from '../extractFirstNameFromDeviceNam
 test('parses common name without last name correctly from user device name', () => {
   const deviceName = 'Julia\'s iPhone';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses common name without last name correctly from user device name', () => {
   const deviceName = 'julia\'s iPhone';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses common name without last name correctly from user device name', () => {
   const deviceName = 'julia\'s';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses common name without last name correctly from user device name', () => {
   const deviceName = 'julia';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses common name without last name correctly from user device name', () => {
   const deviceName = 'julia qiu';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses uncommon name correctly from user device name', () => {
   const deviceName = 'Julia\'s Nexus';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses uncommon name correctly from user device name', () => {
   const deviceName = 'Julia Samsung';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses common name with last name correctly from user device name', () => {
   const deviceName = 'Julia Qiu\'s iPhone';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toEqual('Julia');
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toEqual('Julia');
 });
 
 test('parses uncommon name correctly from user device name', () => {
   const deviceName = 'FYB\'s iPhone';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toBeNull();
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toBeNull();
 });
 
 test('parses no name correctly from user device name', () => {
   const deviceName = 'iPhone';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toBeNull();
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toBeNull();
 });
 
 test('parses uncommon name correctly from user device name', () => {
   const deviceName = 'Birdy\'s iPhone';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toBeNull();
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toBeNull();
 });
 
 test('parses uncommon name correctly from user device name', () => {
   const deviceName = 'Nexus';
 
-  const userName = extractFirstNameFromDeviceName(deviceName);
-  expect(userName).toBeNull();
+  const firstName = extractFirstNameFromDeviceName(deviceName);
+  expect(firstName).toBeNull();
+});
+
+test('parses null correctly from user device name', () => {
+  const firstName = extractFirstNameFromDeviceName();
+  expect(firstName).toBeNull();
 });
--- src/workers/notificationsWorker/extractFirstNameFromDeviceName.js
+++ src/workers/notificationsWorker/extractFirstNameFromDeviceName.js
@@ -2,6 +2,9 @@ import femaleNames from 'datasets-female-first-names-en';
 import maleNames from 'datasets-male-first-names-en';
 
 export const extractFirstNameFromDeviceName = (string) => {
+  if (!string) {
+    return null;
+  }
   const trimedString = string.split(' ')[0].split('\'s')[0];
   const femaleName = femaleNames.find(name => trimedString.toLowerCase() === name.toLowerCase());
   const maleName = maleNames.find(name => trimedString.toLowerCase() === name.toLowerCase());
