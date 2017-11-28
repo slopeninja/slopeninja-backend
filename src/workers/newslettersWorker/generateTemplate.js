c97deb09aac6c8ca62920737b1fc05498e3e4e5d
Julia Qiu <juliaqiuxy@gmail.com>
2017-11-27 22:39:09 -0800

    implement newsletter worker

---
+++ src/workers/newslettersWorker/generateTemplate.js
@@ -0,0 +1,122 @@
+const generateHeader = (dateTime) => `
+<mj-section background-color="#FFFFFF" padding="0 0 10px 0" >
+
+  <mj-group width="100%" vertical-align="middle">
+
+    <mj-column width="80%">
+       <mj-text line-height='16px' padding="12px">
+        <p style="font-size: 22px; font-weight: 400; color: #4A4A4A; font-family: Lato, Arial, sans-serif;" >
+          Snow Updates
+        </p>
+        <p style="font-size: 14px; font-weight: 300; color: #9B9B9B; font-family: Lato, Arial, sans-serif;">
+          ${dateTime}
+        </p>
+      </mj-text>
+    </mj-column>
+
+    <mj-column width="20%">
+      <mj-image  align="right" padding="18px 6px 0 0" width="32px" src="http://www.slope.ninja/emailAssets/logo.png"></mj-image>
+    </mj-column>
+
+  </mj-group>
+
+</mj-section>
+`;
+
+const generateFooter = () => `
+<mj-section background-color="#1ED2FF" background-repeat="repeat" padding="10px 0 0 0" text-align="center" vertical-align="top">
+
+  <mj-group width="100%">
+    <mj-column>
+      <mj-image align="right" padding="24px 10px 0 0" width="110px" height="36px" src="http://www.slope.ninja/emailAssets/appStore.png" href="https://itunes.apple.com/us/app/slope-ninja/id1297809634?ls=1&mt=8">
+      </mj-image>
+
+    </mj-column>
+     <mj-column>
+      <mj-image align="left" padding="24px 0 0 10px" width="110px" height="36px" src="http://www.slope.ninja/emailAssets/playStore.png" href="https://play.google.com/store/apps/details?id=ninja.slope.app"></mj-image>
+    </mj-column>
+  </mj-group>
+
+  <mj-column width="100%">
+    <mj-text align="center" color="#ffffff" font-family="Lato, Arial, sans-serif" font-size="16px" font-weight="300" line-height="22px" padding="22px 25px 10px 25px">
+      (c) Slope Ninja. All rights reserved.
+    </mj-text>
+
+    <mj-text align="left" color="#ffffff" font-family="Lato, Arial, sans-serif" font-size="12px" font-weight="300" line-height="16px" padding="10px 12px 0 12px">
+      You receive this email because you have once subscribed to snow alerts on Slope Ninja.
+    </mj-text>
+    <mj-text align="left" color="#ffffff" font-family="Lato, Arial, sans-serif" font-size="12px" font-weight="300" line-height="16px" padding="10px 12px 0 12px">
+     <a style="color:#ffffff" href="*|UNSUB|*">Unsubscribe</a>
+    </mj-text>
+
+  </mj-column>
+
+   <mj-group width="100%">
+    <mj-column>
+      <mj-image align="right" padding="0 0 0 0" width="110px" src="http://www.slope.ninja/emailAssets/moutains.png"></mj-image>
+    </mj-column>
+  </mj-group>
+
+</mj-section>
+`;
+
+const generateRows = (resortRows) => resortRows.map(row => `
+  <tr style="border-bottom: 1px solid #EDEDED;">
+    <td style="padding: 12px 0 6px 0;">
+      <img style="width: 22px" src=${row.resortLogoUrl} />
+    </td>
+    <td style="padding: 12px 0 6px 0;">
+      <a style="font-size: 14px; font-weight: 400; color: #4A4A4A; font-family: Lato, Arial, sans-serif;" href="${row.resortUrl}" target="_blank">${row.resortName}</a>
+    </td>
+    <td style="padding: 12px 0 6px 0; font-size: 16px; font-weight: 300; color: #4A4A4A; font-family: Lato, Arial, sans-serif;">${row.newSnow}</td>
+    <td style="padding: 12px 0 6px 0; font-size: 16px; font-weight: 300; color: #4A4A4A; font-family: Lato, Arial, sans-serif;">${row.snowDepth}</td>
+    <td style="padding: 12px 0 6px 0; text-align:right;">
+      <img style="width: 22px" src=${row.weatherIconUrl} />
+    </td>
+  </tr>
+`).join('');
+
+const generateBody = (resortRows) => `
+<mj-section background-color="#FFF" background-repeat="repeat" padding="0px 0 40px 0" text-align="center" vertical-align="top">
+
+  <mj-column width="100%">
+    <mj-table padding="0 12px 0 12px">
+      <tr style="text-align:left; padding:0 0 0 0;">
+        <th style="padding: 0 0 0 0;"></th>
+        <th style="padding: 0 0 0 0;"></th>
+        <th style="padding: 0 0 0 0; font-size: 12px; font-weight: 400; color: #9B9B9B; font-family: Lato, Arial, sans-serif;">NEW</th>
+        <th style="padding: 0 0 0 0; font-size: 12px; font-weight: 400; color: #9B9B9B; font-family: Lato, Arial, sans-serif;">DEPTH</th>
+        <th style="padding: 0 0 0 0;"></th>
+      </tr>
+
+      ${generateRows(resortRows)}
+
+    </mj-table>
+  </mj-column>
+
+</mj-section>
+`;
+
+export default (subject, headerAccentColor = '#1ED2FF', dateTime = 'Today 1:00 PM', resortRows = []) => `
+<mjml>
+  <mj-head>
+    <mj-title>{subject}</mj-title>
+    <mj-font name="Lato" href="https://fonts.googleapis.com/css?family=Lato:300,400"/>
+  </mj-head>
+  <mj-body>
+    <mj-container>
+
+      <mj-section padding="0">
+        <mj-column width="100%">
+          <mj-divider width="100%" padding="0" border-color="${headerAccentColor}"></mj-divider>
+        </mj-column>
+      </mj-section>
+
+      ${generateHeader(dateTime)}
+      ${generateBody(resortRows)}
+      ${generateFooter()}
+
+    </mj-container>
+  </mj-body>
+</mjml>
+`;
---
+++ src/workers/newslettersWorker/index.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import { mjml2html } from 'mjml'
+import fetch from 'isomorphic-fetch';
+import ResortService from '../../services/ResortService';
+
+import generateEmail from './generateTemplate';
+import moment from 'moment';
+
+const MAILCHIMP_PRIVATE_KEY = process.env.MAILCHIMP_PRIVATE_KEY;
+
+const EMAIL_ASSETS_BASE_URL = 'http://www.slope.ninja/emailAssets';
+
+const RESORT_LOGOS = {
+  'squaw-valley': `${EMAIL_ASSETS_BASE_URL}/resortLogos/squaw.png`,
+  'alpine-meadows': `${EMAIL_ASSETS_BASE_URL}/resortLogos/squaw.png`,
+  'boreal': `${EMAIL_ASSETS_BASE_URL}/resortLogos/boreal.png`,
+  'diamond-peak': `${EMAIL_ASSETS_BASE_URL}/resortLogos/diamond.png`,
+  'donner-ski-ranch': `${EMAIL_ASSETS_BASE_URL}/resortLogos/donner.png`,
+  'heavenly': `${EMAIL_ASSETS_BASE_URL}/resortLogos/heavenly.png`,
+  'homewood': `${EMAIL_ASSETS_BASE_URL}/resortLogos/homewood.png`,
+  'kirkwood': `${EMAIL_ASSETS_BASE_URL}/resortLogos/kirkwood.png`,
+  'mt-rose': `${EMAIL_ASSETS_BASE_URL}/resortLogos/mt-rose.png`,
+  'northstar': `${EMAIL_ASSETS_BASE_URL}/resortLogos/northstar.png`,
+  'sierra-at-tahoe': `${EMAIL_ASSETS_BASE_URL}/resortLogos/sierra.png`,
+  'sugar-bowl': `${EMAIL_ASSETS_BASE_URL}/resortLogos/sugarbowl.png`,
+};
+
+const WEATHER_ICONS = {
+  sunny: `${EMAIL_ASSETS_BASE_URL}/weatherIcons/sunny.png`,
+  clear: `${EMAIL_ASSETS_BASE_URL}/weatherIcons/clear.png`,
+  cloudy: `${EMAIL_ASSETS_BASE_URL}/weatherIcons/cloudy.png`,
+  thunderstorm: `${EMAIL_ASSETS_BASE_URL}/weatherIcons/thunderstorm.png`,
+  rain: `${EMAIL_ASSETS_BASE_URL}/weatherIcons/rain.png`,
+  snow: `${EMAIL_ASSETS_BASE_URL}/weatherIcons/snow.png`,
+};
+
+const COLOR_PALETTE = [
+  '#1ED2FF',
+  '#FFB061',
+  '#4CB950',
+  '#FF9274',
+  '#E5437C',
+  '#736363',
+  '#FFCF61',
+];
+
+const makeMailchimpHappy = (rawHtml) => rawHtml.replace('@import url(https://fonts.googleapis.com/css?family=Lato:300,400);', '').replace('@import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);', '')
+
+const run = async () => {
+  const resortService = new ResortService();
+  const resorts = await resortService.getResorts();
+  const resortRows = resorts.map(resort => {
+    return {
+      resortName: resort.name,
+      resortUrl: `http://slope.ninja/resorts/${resort.shortName}`,
+      newSnow: resort.weather.newSnow !== null ? `${resort.weather.newSnow}"` : '-',
+      snowDepth: resort.weather.snowDepth !== null ? `${resort.weather.snowDepth}"` : '-',
+      weatherIconUrl: WEATHER_ICONS[resort.weather.condition],
+      resortLogoUrl: RESORT_LOGOS[resort.shortName],
+    };
+  });
+
+  resortRows.sort((a, b) => {
+    if (a.snowDepth < b.snowDepth) {
+      return 1;
+    }
+    if (a.snowDepth > b.snowDepth) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  resortRows.sort((a, b) => {
+    if (a.newSnow < b.newSnow) {
+      return 1;
+    }
+    if (a.newSnow > b.newSnow) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  const now = moment();
+  const currentDateFormatted = now.format('MMMM Do, YYYY');
+  const dateTimeFormatted = `${currentDateFormatted} 1:00 PM`;
+
+  const weekOfDay = now.weekday();
+
+
+  const SUBJECT_LINE = `Snow Update — ${currentDateFormatted}`;
+
+  const mjml = generateEmail(SUBJECT_LINE, COLOR_PALETTE[weekOfDay], dateTimeFormatted, resortRows);
+  const html = mjml2html(mjml)
+
+  const rawHtml = makeMailchimpHappy(html.html)
+
+  // Forward the post reqest to MailChimp api
+  const token = new Buffer(`anystring:${MAILCHIMP_PRIVATE_KEY}`).toString('base64');
+
+  const TEMPLATE_FOLDER_ID = 'd01c23837c'; /* Daily Snow Updates Templates */
+  const CAMPAIGN_FOLDER_ID = '37045c44fa'; /* Daily Snow Updates */
+  const LIST_ID = 'b56b3d32c5';
+
+  const templateCreationRequest = await fetch(`http://us15.api.mailchimp.com/3.0/templates`, {
+    method: 'POST',
+    headers: new Headers({
+      authorization: `Basic ${token}`,
+      'content-type': 'application/json',
+    }),
+    body: JSON.stringify({
+      name: SUBJECT_LINE,
+      folder_id: TEMPLATE_FOLDER_ID,
+    	html: rawHtml,
+    }),
+  });
+
+  const templateCreationResponseBody = await templateCreationRequest.json();
+  const templateId = templateCreationResponseBody.id;
+
+  const campaignCreationRequest = await fetch(`http://us15.api.mailchimp.com/3.0/campaigns`, {
+    method: 'POST',
+    headers: new Headers({
+      authorization: `Basic ${token}`,
+      'content-type': 'application/json',
+    }),
+    body: JSON.stringify({
+    	'type': 'regular',
+    	'recipients': {
+    		'list_id': LIST_ID,
+    	},
+    	'settings': {
+    		'subject_line': SUBJECT_LINE,
+    		'preview_text': '',
+    		'title': SUBJECT_LINE,
+    		'from_name': 'Slope Ninja',
+    		'reply_to': 'donotreply@slope.ninja',
+    		'use_conversation':  false,
+    		'to_name': '',
+    		'folder_id': CAMPAIGN_FOLDER_ID,
+    		'authenticate': true,
+    		'auto_footer': false,
+    		'inline_css': false,
+    		'auto_tweet': false,
+    		'fb_comments': false,
+    		'template_id': templateId,
+    	},
+    	'tracking': {
+    		'opens': true,
+    		'html_clicks': true,
+    		'text_clicks': false,
+    		'goal_tracking': false,
+    		'ecomm360': false,
+    	},
+    }),
+  });
+
+  const campaignCreationResponseBody = await campaignCreationRequest.json();
+  const campaignId = campaignCreationResponseBody.id;
+
+  const campaignDeliveryRequest = await fetch(`http://us15.api.mailchimp.com/3.0/campaigns/${campaignId}/actions/send`, {
+    method: 'POST',
+    headers: new Headers({
+      authorization: `Basic ${token}`,
+    }),
+  });
+
+  const campaignDeliverResponseBody = await campaignDeliveryRequest.json();
+  console.log(campaignDeliverResponseBody);
+
+  console.log(`Campaign Sent ${campaignId}:${templateId}`);
+}
+
+run();
