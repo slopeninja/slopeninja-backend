7546f714698d35aefd7fc67047cef34dfef90e9d
Julia Qiu <juliaqiuxy@gmail.com>
2018-01-19 13:23:01 -0800

    run parser more frequently

--- src/workers/scheduler.js
+++ src/workers/scheduler.js
@@ -8,13 +8,14 @@ import { run as runNotificationsWorkerPM } from './notificationsWorker/index-pm'
 
 const TIMEZONE = 'America/Los_Angeles';
 const EVERY_SECOND = '* * * * * *';
+const EVERY_HALF_HOUR = '*/30 * * * *';
 const EVERY_HOUR = '0 * * * *';
 const EVERYDAY_AT_1_PM = '0 13 * * *';
 const EVERYDAY_AT_5_AM = '0 5 * * *';
 const EVERYDAY_AT_5_PM = '0 17 * * *';
 
 const parserJob = new CronJob(
-  EVERY_HOUR,
+  EVERY_HALF_HOUR,
   runParseWorker,
   null,
   false, /* Start the job right now */
