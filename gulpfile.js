f4ece81cf2fe0b74bfffc995addb84a885c18be3
Julia Qiu <juliaqiuxy@gmail.com>
2018-01-19 13:25:43 -0800

    clean up deps

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-
-gulp.task('default', () => {
-  return gulp.src(['app.js', 'src/**/*.js'])
-    .pipe(babel())
-    .pipe(gulp.dest(dist));
-})
