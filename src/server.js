2cbe218cc7d36db2d9ca50073a80b851ae417100
Julia Qiu <juliaqiuxy@gmail.com>
2018-01-19 14:24:26 -0800

    add sentry error catching

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import api from './api';
 import * as scheduler from './workers/scheduler';
+import Raven from 'raven';
+
+Raven.config(process.env.SENTRY_URL).install();
 
 const PORT = process.env.PORT || 8080;
 
 api.listen(PORT, () => {
-  console.info(`Listening to http://localhost:${PORT}`);
+  console.info(`Listening on ${PORT}`);
 });
 
 if (process.env.NODE_ENV === 'production') {
